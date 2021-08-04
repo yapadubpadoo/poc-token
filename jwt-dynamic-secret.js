b161c60e42c6ac664b7635ddfec963cd80b8de8f
Nuttapon.Y <nuttapon@appsynth.net>
2021-08-04 11:59:18 +0700

    add POC scripts

---
+++ jwt-dynamic-secret.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const main = async () => {
+    const next10Seconds = Math.floor(Date.now() / 1000) + (10)
+    
+    const secret = 'a-secret-between-you-and-me';
+    const data = { hello: "world", transactionTime: new Date() }
+
+    // we will use a part of data to make the secret dynamic
+    const dynamicSecret = Buffer.from(JSON.stringify(data)).toString('base64').slice(7, 21)
+    
+    const token = jwt.sign({
+        exp: next10Seconds,
+        data
+    }, `${secret}-${dynamicSecret}`, { algorithm: 'HS256' });
+    console.log("Token:", token)
+    console.log('Decoded', jwt.verify(token, `${secret}-${dynamicSecret}`));
+    console.log('Decoded', jwt.verify(token, `${secret}`));
+}
+
+main()
---
+++ jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const sleepSec = async (seconds) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000);
+    });
+}
+
+const main = async () => {
+    const next10Seconds = Math.floor(Date.now() / 1000) + (10)
+    
+    const secret = 'a-secret-between-you-and-me';
+    const data = { hello: "world" }
+    
+    const token = jwt.sign({
+        exp: next10Seconds,
+        data
+    }, secret, { algorithm: 'HS256' });
+
+    console.log(token)
+    for(let i = 1; i<100; i++) {
+        const decoded = jwt.verify(token, secret);
+        const now = new Date()
+        const tokenExpire = new Date(decoded.exp * 1000)
+        console.log(now, tokenExpire, now >= tokenExpire, decoded)
+        await sleepSec(1)
+    }
+}
+
+main()
